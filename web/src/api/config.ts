eb5c172d2f2fab8b503ed76107e75efcc73f09ef
Ihor Pankiv <harry.pankiv@gmail.com>
2024-08-22 11:07:46 +0300

    fix: changed env variable (append VITE_)

--- web/src/api/config.ts
+++ web/src/api/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.BACKEND_URL || 'http://localhost:3000';
+const baseURL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
 
 export const axiosInstance = axios.create({
   baseURL: baseURL,
